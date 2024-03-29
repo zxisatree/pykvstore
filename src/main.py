f07234ed7c3a95faa86425a17e5152ea878e1bcb
zxisatree <62459441+zxisatree@users.noreply.github.com>
2024-03-29 10:38:58 +0800

    Loop response

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, addr = server_socket.accept()  # wait for client
     with conn:
+        # loop
         while True:
             data = conn.recv(1024)
             if not data:
