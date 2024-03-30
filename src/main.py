45731164ae653e509849686ade9eed6fc00e664c
zxisatree <62459441+zxisatree@users.noreply.github.com>
2024-03-30 10:59:39 +0800

    Implement port handling

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import argparse
 import socket
 import threading
 
@@ -12,10 +13,12 @@ import database
 
 
 def main():
+    argparser = setup_argpaser()
+    args = argparser.parse_args()
     db = database.Database()
     try:
         socket.setdefaulttimeout(30)
-        server_socket = socket.create_server(("localhost", 6379))
+        server_socket = socket.create_server(("localhost", args.port))
         while True:
             print("main thread waiting...")
             conn, addr = server_socket.accept()
@@ -34,6 +37,16 @@ def handle_conn(conn: socket.socket, addr, db: database.Database):
             cmd = codec.parse_cmd(data)
             conn.sendall(cmd.execute(db).encode())
         print(f"Connection closed: {addr=}")
+
+
+def setup_argpaser() -> argparse.ArgumentParser:
+    argparser = argparse.ArgumentParser(
+        prog="Readthis", description="Redis clone for Windows"
+    )
+    argparser.add_argument(
+        "--port", type=int, default=6379, help="Port to listen on (default: 6379)"
+    )
+    return argparser
 
 
 if __name__ == "__main__":
